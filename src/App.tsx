104093b82c28a3b5c4bb1ad4a90f1b2332913c12
kavalcio <sarpkavalcioglu@gmail.com>
2024-12-15 20:30:36 -0500

    Initialize instant db integration

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,67 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { init, tx, id, lookup } from '@instantdb/react'
+
+// DO NOT COMMIT
+const APP_ID = 'asd'
+
+type Habit = {
+  id: string;
+  activity: string;
+  filled: boolean;
+  date: string;
+  createdAt: string;
+}
+
+type User = {
+  id: string;
+  name: string;
+  email: string;
+}
+
+type Schema = {
+  users: User;
+  habits: Habit;
+}
+
+const db = init<Schema>({ appId: APP_ID });
+
 function App() {
   const [count, setCount] = useState(0)
 
+  const { user } = db.useAuth();
+
+  const { data, isLoading, error } = db.useQuery({
+    habits: {},
+  });
+
+  console.log({data, isLoading, error});
+
+  // console.log(new Date().toLocaleDateString("en-CA", {year:"numeric", month: "2-digit", day:"2-digit"}));
+  console.log(new Date().toISOString());
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      {data?.habits?.map((habit: any) => (
+        <button onClick={() => {
+          db.transact([tx.habits[habit.id].merge({
+            filled: !habit.filled,
+          })]);
+        }}>
+          <p key={habit.id}>{habit.activity} - {habit.filled ? 'filled' : 'not filled'} - date: {habit.date}</p>
+        </button>
+      ))}
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => {
+          setCount((count) => count + 1);
+          const date = new Date();
+          db.transact([tx.habits[id()].update({
+            activity: 'workout',
+            filled: true,
+            date: date.toLocaleDateString("en-CA", {year:"numeric", month: "2-digit", day:"2-digit"}),
+            createdAt: date.toISOString(),
+          })]);
+        }}>
           count is {count}
         </button>
         <p>
