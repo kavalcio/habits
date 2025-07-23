3df7a2a417015821cfd361a7fcf955b518967828
kavalcio <sarpkavalcioglu@gmail.com>
2025-07-23 18:54:33 -0400

    Add dialog for habit management

---
+++ src/components/AddEditHabitDialog.tsx
@@ -0,0 +1,87 @@
+import { Button, Dialog, Flex, Heading, TextField } from '@radix-ui/themes';
+import { useMutation } from '@tanstack/react-query';
+import { useState } from 'react';
+import { HexColorPicker } from 'react-colorful';
+import { useNavigate } from 'react-router-dom';
+import { Habit } from 'src/types';
+
+import { Routes } from '@/constants';
+import { createHabit, deleteHabit, updateHabit } from '@/requests';
+
+export const AddEditHabitDialog = ({
+  children,
+  habit,
+}: {
+  children: React.ReactNode;
+  habit?: Habit;
+}) => {
+  const navigate = useNavigate();
+
+  const [habitName, setHabitName] = useState<string>();
+  const [habitColor, setHabitColor] = useState<string>();
+
+  const createHabitMutation = useMutation(createHabit);
+  const updateHabitMutation = useMutation(updateHabit);
+  const deleteHabitMutation = useMutation(deleteHabit);
+
+  const onCreateHabit = async () => {
+    await createHabitMutation.mutateAsync({
+      name: habitName,
+      color: habitColor,
+    });
+  };
+
+  const onUpdateHabit = async () => {
+    // TODO: validate inputs
+    await updateHabitMutation.mutateAsync({
+      id: habit.id!,
+      name: habitName!,
+      color: habitColor!,
+    });
+  };
+
+  const onDeleteHabit = async () => {
+    await deleteHabitMutation.mutateAsync(habit.id);
+    navigate(Routes.DASHBOARD, { replace: true });
+  };
+
+  return (
+    <Dialog.Root>
+      <Dialog.Content maxWidth="400px">
+        <Flex direction="column">
+          <Dialog.Title mr="auto">Edit Habit</Dialog.Title>
+          <Heading size="2" mr="auto">
+            Name
+          </Heading>
+          <TextField.Root
+            placeholder="Habit Name"
+            defaultValue={habit.name}
+            onChange={(e) => setHabitName(e.target.value)}
+            mt="1"
+          />
+          <Heading size="2" mt="3" mb="1" mr="auto">
+            Color
+          </Heading>
+          <HexColorPicker color={habitColor} onChange={setHabitColor} />
+          {/* <Card mt="3">
+            <Heading color="red" size="2" mb="2">
+              Danger Zone
+            </Heading>
+            <Button>Delete Habit</Button>
+          </Card> */}
+          <Flex width="100%" justify="end" gap="2" mt="4">
+            <Dialog.Close>
+              <Button variant="soft">Cancel</Button>
+            </Dialog.Close>
+            <Dialog.Close>
+              <Button onClick={habit ? onUpdateHabit : onCreateHabit}>
+                Save
+              </Button>
+            </Dialog.Close>
+          </Flex>
+        </Flex>
+      </Dialog.Content>
+      {children}
+    </Dialog.Root>
+  );
+};
--- src/pages/HabitOverview/HabitOverview.tsx
+++ src/pages/HabitOverview/HabitOverview.tsx
@@ -1,22 +1,20 @@
+import { Pencil1Icon } from '@radix-ui/react-icons';
 import {
-  Button,
   Container,
+  Dialog,
   Flex,
   Heading,
-  TextField,
+  IconButton,
   Theme,
 } from '@radix-ui/themes';
-import { useMutation, useQuery } from '@tanstack/react-query';
-import { useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
 
-import { YearGrid } from '@/components';
-import { Routes } from '@/constants';
-import { deleteHabit, fetchEvents, fetchHabit, updateHabit } from '@/requests';
+import { AddEditHabitDialog, YearGrid } from '@/components';
+import { fetchEvents, fetchHabit } from '@/requests';
 
 // TODO: add custom theme override on this page that uses the habit color
 export const HabitOverview = () => {
-  const navigate = useNavigate();
   const { habitId } = useParams();
 
   const { data, error, isPending } = useQuery(fetchHabit(habitId));
@@ -24,56 +22,26 @@ export const HabitOverview = () => {
 
   const habit = data?.[0];
 
-  const [habitName, setHabitName] = useState<string>();
-  const [habitColor, setHabitColor] = useState<string>();
-
-  const updateHabitMutation = useMutation(updateHabit);
-  const deleteHabitMutation = useMutation(deleteHabit);
-
   // TODO: improve error and loading states
   if (isPending) return <div>loading...</div>;
   if (error) return <div>error: {error.message}</div>;
   if (!habit || !habitId) return <div>habit not found</div>;
 
-  const onUpdateHabit = async () => {
-    // TODO: validate inputs
-    await updateHabitMutation.mutateAsync({
-      id: habitId!,
-      name: habitName!,
-      color: habitColor!,
-    });
-  };
-
-  const onDeleteHabit = async () => {
-    await deleteHabitMutation.mutateAsync(habitId!);
-    navigate(Routes.DASHBOARD, { replace: true });
-  };
-
   return (
     <Theme accentColor="red">
       <Container>
         <Flex gap="2" direction="column">
-          <Heading size="4">Habit: {habit.name}</Heading>
+          <Flex width="100%" align="center" gap="3">
+            <Heading size="4">Habit: {habit.name}</Heading>
+            <AddEditHabitDialog habit={habit}>
+              <Dialog.Trigger>
+                <IconButton size="1" variant="ghost">
+                  <Pencil1Icon />
+                </IconButton>
+              </Dialog.Trigger>
+            </AddEditHabitDialog>
+          </Flex>
           <YearGrid habitId={habitId} events={events} />
-          <TextField.Root
-            placeholder="Habit Name"
-            onChange={(e) => setHabitName(e.target.value)}
-          />
-          <input
-            type="color"
-            placeholder="habit color"
-            onChange={(e) => setHabitColor(e.target.value)}
-          />
-          <Button
-            variant="surface"
-            onClick={onUpdateHabit}
-            disabled={!habitName || !habitColor}
-          >
-            update habit
-          </Button>
-          <Button variant="soft" onClick={onDeleteHabit}>
-            delete habit
-          </Button>
         </Flex>
       </Container>
     </Theme>
