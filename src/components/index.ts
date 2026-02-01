39cfe1d3b81388c84cbd67f4d59385fe90017321
kavalcio <sarpkavalcioglu@gmail.com>
2026-02-01 15:43:40 -0500

    Remove broken export

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,5 +6,4 @@ export * from './EditEventDialog';
 export * from './EventCalendar';
 export * from './FormError';
 export * from './Header';
-export * from './ScrollingActivity';
 export * from './YearGrid';
