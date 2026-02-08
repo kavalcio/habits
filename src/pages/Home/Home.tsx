a2596a36b247fbb652c28f4a0f1effb58eab7c68
kavalcio <sarpkavalcioglu@gmail.com>
2026-02-08 14:45:05 -0500

    Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,69 @@
-import { Container } from '@radix-ui/themes';
+import {
+  Button,
+  Container,
+  Flex,
+  Heading,
+  Separator,
+  Text,
+} from '@radix-ui/themes';
+import { useQuery } from '@tanstack/react-query';
+import { Link as RouterLink } from 'react-router-dom';
 
 import { AbacusAnimation } from '@/components';
+import { Routes } from '@/constants';
+import { fetchSession } from '@/requests';
 
 export const Home = () => {
+  const { data: session, isPending } = useQuery(fetchSession);
+
   return (
-    <Container size="3">
-      <AbacusAnimation />
+    <Container size="3" pt="6">
+      <Flex
+        direction={{
+          initial: 'column',
+          sm: 'row',
+        }}
+        gap="8"
+      >
+        <Flex direction="column" gap="5" flexGrow="1">
+          <Heading size="6">Welcome to Temper</Heading>
+          <Text size="3" color="gray">
+            Temper is a habit tracking app designed to help you build, monitor,
+            and maintain positive routines. Track your progress, visualize your
+            streaks, and stay motivated every day.
+          </Text>
+          <Separator style={{ width: '100%' }} />
+          {!isPending && (
+            <Flex direction="row" gap="4" align="center" justify="center">
+              {session ? (
+                <>
+                  <Button asChild variant="solid">
+                    <RouterLink to={Routes.DASHBOARD}>
+                      <Text>Go to Dashboard</Text>
+                    </RouterLink>
+                  </Button>
+                </>
+              ) : (
+                <>
+                  <Button asChild variant="solid">
+                    <RouterLink to={Routes.LOGIN}>
+                      <Text>Log In</Text>
+                    </RouterLink>
+                  </Button>
+                  <Button asChild variant="outline">
+                    <RouterLink to={Routes.REGISTER}>
+                      <Text>Sign Up</Text>
+                    </RouterLink>
+                  </Button>
+                </>
+              )}
+            </Flex>
+          )}
+        </Flex>
+        <Flex flexShrink="1">
+          <AbacusAnimation />
+        </Flex>
+      </Flex>
     </Container>
   );
 };
